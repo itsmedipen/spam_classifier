7580758dd334c50417aea6d76491d02052aa5a62
Dipen Sherpa <technolove851@gmail.com>
2025-09-02 16:40:35 +0545

    change in app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import nltk
-# Download required NLTK data files (only if not already present)
-nltk.download('punkt', quiet=True)
-nltk.download('stopwords', quiet=True)
-
 
 import streamlit as st
 import pickle
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 import string
+
+import nltk
+
+# Download NLTK data before it's used
+nltk.download('punkt')
+nltk.download('stopwords')
 
 snow = SnowballStemmer('english')
 
